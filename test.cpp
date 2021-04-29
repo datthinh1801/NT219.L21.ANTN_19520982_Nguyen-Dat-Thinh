b0d3bf4b732a4ee0157af6a772adf36baef63738
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-04-29 07:43:44 +0700

    Used for debugging

--- test.cpp
+++ test.cpp
@@ -14,7 +14,7 @@ void printBits(vector<unsigned char> block)
     cout << endl;
 }
 
-vector<unsigned char> Split(vector<unsigned char> block, unsigned char start, unsigned char len)
+vector<unsigned char> Split(vector<unsigned char> block, unsigned int start, unsigned char len)
 {
     vector<unsigned char> sub_bits(len);
     for (int i = 0; i < len; ++i)
@@ -47,24 +47,10 @@ unsigned char ConvertBinToDec(vector<unsigned char> block)
     return result;
 }
 
-vector<unsigned char> ConvertStringToBin(string str)
-{
-    vector<unsigned char> result;
-    for (int i = 0; i < str.length(); ++i)
-    {
-        vector<unsigned char> single_char = ConvertDecToBin(int(str[i]), 8);
-        for (int j = 0; j < 8; ++j)
-        {
-            result.push_back(single_char[j]);
-        }
-    }
-
-    return result;
-}
-
 string ConvertBinToString(vector<unsigned char> block)
 {
     string result = "";
+
     for (int i = 0; i < block.size(); i += 8)
     {
         vector<unsigned char> sub_block = Split(block, i, 8);
@@ -74,68 +60,42 @@ string ConvertBinToString(vector<unsigned char> block)
     return result;
 }
 
-vector<unsigned char> GenerateKeyRandomly()
+// Fine
+vector<unsigned char> ConvertStringToBin(string str)
 {
-    vector<unsigned char> key(64);
-
-    // Generate the key
-    for (int i = 0; i < 8; ++i)
+    vector<unsigned char> result;
+    for (int i = 0; i < str.length(); ++i)
     {
-        // Random an 8-bit number
-        unsigned char random_value = rand() % 256;
+        vector<unsigned char> single_char = ConvertDecToBin(str[i], 8);
 
-        // Convert the random number to binary
-        vector<unsigned char> bits = ConvertDecToBin(random_value, 8);
-
-        // Assign bits from the random number to the key
-        for (int j = 0; j < 8; ++j)
+        for (int j = 0; j < single_char.size(); ++j)
         {
-            key[i * 8 + j] = bits[j];
+            result.push_back(single_char[j]);
         }
     }
 
-    return key;
-}
-
-vector<unsigned char> DropParityBit(vector<unsigned char> block)
-{
-    // Remove bit from tail to head in order not to confuse the offset while removing
-    for (int i = 63; i >= 0; i -= 8)
-    {
-        block.erase(block.begin() + i);
-    }
-
-    return block;
-}
-
-vector<unsigned char> PermutedChoice1(vector<unsigned char> block)
-{
-    const unsigned char pc1[56] = {
-        57, 49, 41, 33, 25, 17, 9,
-        1, 58, 50, 42, 34, 26, 18,
-        10, 2, 59, 51, 43, 35, 27,
-        19, 11, 3, 60, 52, 44, 36,
-        63, 55, 47, 39, 31, 23, 15,
-        7, 62, 54, 46, 38, 30, 22,
-        14, 6, 61, 53, 45, 37, 29,
-        21, 13, 5, 28, 20, 12, 4};
-
-    vector<unsigned char> permuted_key(56);
-    for (int i = 0; i < 56; ++i)
-    {
-        permuted_key[i] = block[pc1[i]];
-    }
-    return permuted_key;
+    return result;
 }
 
 int main()
 {
-    srand(time(0));
-    auto key = GenerateKeyRandomly();
-    cout << key.size() << endl;
-    // auto dropped_key = DropParityBit(key);
-    // cout << dropped_key.size() << endl;
-    auto pc1_key = PermutedChoice1(key);
-    printBits(pc1_key);
+    string s = "mot hai ba bon nam sau bay tam chin";
+    auto block = ConvertStringToBin(s);
+    for (int i = 0; i < block.size(); ++i)
+    {
+        if (i % 8 == 0)
+        {
+            cout << endl;
+            system("pause");
+            // Bug
+            cout << "#" << i << ":\n";
+            auto subb = Split(block, i, 8);
+            printBits(subb);
+        }
+
+        cout << (unsigned int)block[i] << " ";
+    }
+    // string r = ConvertBinToString(block);
+    // cout << "result: " << r << endl;
     return 0;
 }
