cd94720702ea03491a5603ef980325d7b7523315
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-05-27 21:31:33 +0700

    Initial commit

--- Lab 3-4/RSA.cpp
+++ Lab 3-4/RSA.cpp
@@ -1,35 +1,33 @@
 // Sample.cpp
 
-#include "stdafx.h"
-
-#include "rsa.h"
-using CryptoPP::RSA;
+#include "cryptopp/rsa.h"
 using CryptoPP::InvertibleRSAFunction;
-using CryptoPP::RSAES_OAEP_SHA_Encryptor;
+using CryptoPP::RSA;
 using CryptoPP::RSAES_OAEP_SHA_Decryptor;
+using CryptoPP::RSAES_OAEP_SHA_Encryptor;
 
-#include "sha.h"
+#include "cryptopp/sha.h"
 using CryptoPP::SHA1;
 
-#include "filters.h"
+#include "cryptopp/filters.h"
+using CryptoPP::PK_DecryptorFilter;
+using CryptoPP::PK_EncryptorFilter;
 using CryptoPP::StringSink;
 using CryptoPP::StringSource;
-using CryptoPP::PK_EncryptorFilter;
-using CryptoPP::PK_DecryptorFilter;
 
-#include "files.h"
+#include "cryptopp/files.h"
 using CryptoPP::FileSink;
 using CryptoPP::FileSource;
 
-#include "osrng.h"
+#include "cryptopp/osrng.h"
 using CryptoPP::AutoSeededRandomPool;
 
-#include "SecBlock.h"
+#include "cryptopp/SecBlock.h"
 using CryptoPP::SecByteBlock;
 
-#include "cryptlib.h"
-using CryptoPP::Exception;
+#include "cryptopp/cryptlib.h"
 using CryptoPP::DecodingResult;
+using CryptoPP::Exception;
 
 #include <string>
 using std::string;
@@ -38,13 +36,13 @@ using std::string;
 using std::exception;
 
 #include <iostream>
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 #include <assert.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     try
     {
@@ -53,44 +51,41 @@ int main(int argc, char* argv[])
         AutoSeededRandomPool rng;
 
         InvertibleRSAFunction parameters;
-        parameters.GenerateRandomWithKeySize( rng, 1024 );
+        parameters.GenerateRandomWithKeySize(rng, 3072);
 
-        RSA::PrivateKey privateKey( parameters );
-        RSA::PublicKey publicKey( parameters );
+        RSA::PrivateKey privateKey(parameters);
+        RSA::PublicKey publicKey(parameters);
 
-        string plain="RSA Encryption", cipher, recovered;
+        string plain = "RSA Encryption", cipher, recovered;
 
         ////////////////////////////////////////////////
         // Encryption
-        RSAES_OAEP_SHA_Encryptor e( publicKey );
+        RSAES_OAEP_SHA_Encryptor e(publicKey);
 
-        StringSource( plain, true,
-            new PK_EncryptorFilter( rng, e,
-                new StringSink( cipher )
-            ) // PK_EncryptorFilter
-         ); // StringSource
+        StringSource(plain, true,
+                     new PK_EncryptorFilter(rng, e,
+                                            new StringSink(cipher)) // PK_EncryptorFilter
+        );                                                          // StringSource
 
         ////////////////////////////////////////////////
         ////////////////////////////////////////////////
 
         ////////////////////////////////////////////////
         // Decryption
-        RSAES_OAEP_SHA_Decryptor d( privateKey );
+        RSAES_OAEP_SHA_Decryptor d(privateKey);
 
-        StringSource( cipher, true,
-            new PK_DecryptorFilter( rng, d,
-                new StringSink( recovered )
-            ) // PK_EncryptorFilter
-         ); // StringSource
+        StringSource(cipher, true,
+                     new PK_DecryptorFilter(rng, d,
+                                            new StringSink(recovered)) // PK_EncryptorFilter
+        );                                                             // StringSource
 
-        assert( plain == recovered );
+        assert(plain == recovered);
     }
-    catch( CryptoPP::Exception& e )
+    catch (CryptoPP::Exception &e)
     {
         cerr << "Caught Exception..." << endl;
         cerr << e.what() << endl;
     }
 
-	return 0;
+    return 0;
 }
-
