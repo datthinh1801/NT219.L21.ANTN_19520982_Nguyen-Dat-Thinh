6d72f2790ae90dd36a920416a31c8b034e6d972d
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-06-05 10:37:19 +0700

    Update curve

--- Lab 3-4/ECC_OP.cpp
+++ Lab 3-4/ECC_OP.cpp
@@ -21,87 +21,94 @@ using CryptoPP::ModularSquareRoot;
 
 #include <cryptopp/ecp.h>
 #include <cryptopp/eccrypto.h>
-using CryptoPP::ECP;    // Prime field p
-using CryptoPP::ECIES;
-using CryptoPP::ECPPoint;
+using CryptoPP::DL_FixedBasePrecomputation;
 using CryptoPP::DL_GroupParameters_EC;
 using CryptoPP::DL_GroupPrecomputation;
-using CryptoPP::DL_FixedBasePrecomputation;
+using CryptoPP::ECIES;
+using CryptoPP::ECP; // Prime field p
+using CryptoPP::ECPPoint;
 
 #include <cryptopp/pubkey.h>
-using CryptoPP::PublicKey;
 using CryptoPP::PrivateKey;
+using CryptoPP::PublicKey;
 
 /* standard curves*/
 #include <cryptopp/asn.h>
-#include <cryptopp/oids.h> // 
+#include <cryptopp/oids.h> //
 namespace ASN1 = CryptoPP::ASN1;
 using CryptoPP::OID;
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     AutoSeededRandomPool rng;
-// Contruct  ECP(const Integer &modulus, const FieldElement &A, const FieldElement &B);
+    // Contruct  ECP(const Integer &modulus, const FieldElement &A, const FieldElement &B);
 
-        // User Defined Domain Parameters for curve y^2 =x^3 + ax +b
-        // Modulus p
-        Integer p("0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff");
-        // Coefiction a
-        Integer a("0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc");
-        // Coefiction b
-        Integer b("0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b");
-        /* create a curve*/
-        a %= p;     b %= p; // a mod p, b mod p
-        /* ECC curve */
-        CryptoPP::ECP eqcurve256(p, a, b);
-        /* subgroup <G> on curve */ 
-         // x, y: Base Point G
-        Integer x("0x0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798");
-        Integer y("0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8");
-        // Creat point G
-        ECP::Point G(x,y);
-        // Oder n of group <G>
-        Integer n("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
-        //Cofactors
-         Integer h("0x1");
-        /* Set ECC parameters and subgroup <G>*/
-        // CryptoPP::DL_GroupParameters_EC<ECP> curve256(eqcurve256,G,n,h);
-        CryptoPP::DL_GroupParameters_EC<ECP> curve256;
-        curve256.Initialize(eqcurve256,G,n,h);
-        /* Curve parameters*/
-        cout << "Group order n=" << curve256.GetGroupOrder() << endl;
-        cout << "Cofactor h=" << curve256.GetCofactor() << endl;
-        cout << "Coefficient a=" << eqcurve256.GetA() << endl;
-        cout << "Coefficient b=" << eqcurve256.GetB() << endl;
-        cout << "Gx=" << G.x <<endl;
-        cout << "Gy=" << G.y <<endl;
-        /* Curve operations*/
+    // Define curve's parameters
+    // Modulus p
+    Integer p("0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF");
+    // Coefiction a
+    Integer a("0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC");
+    // Coefiction b
+    Integer b("0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B");
 
-        /* Compute on subgroup <G> */
-        ECP::Point  Q=curve256.GetCurve().Double(G);
-        cout << "Qx=" << Q.x << endl;
-        cout << "Qy=" << Q.y << endl;
-        // Scalar Multiply
-        CryptoPP::Integer k("871.");
-        ECP::Point U=curve256.GetCurve().Multiply(k,G);
-        cout << "Ux=" << U.x << endl;
-        cout << "Uy=" << U.y << endl;
-        // Point Addition
-        ECP::Point V=curve256.GetCurve().Add(Q,U);
-        cout << "Vx=" << U.x << endl;
-        cout << "Vy=" << U.y << endl;
-         // Point invertion
-        ECP::Point X=curve256.GetCurve().Inverse(G);
-        cout << "Xx=" << X.x << endl;
-        cout << "Xy=" << X.y << endl;
-        // Multiple
-        ECP::Point H=curve256.GetCurve().ScalarMultiply(G,k);
-        cout << "Hx=" << H.x << endl;
-        cout << "Hy=" << H.y << endl;
+    // a mod p
+    a %= p;
+    // b mod p
+    b %= p;
 
-        /* Compute on ECC curve  y^2 = x^3 +ax +b*/
-        ECP::Point I=eqcurve256.Add(G,H);
-        cout << "Hx=" << I.x << endl;
-        cout << "Hy=" << I.y << endl;
-	}
+    // CREATE A CURVE
+    CryptoPP::ECP eqcurve256r1(p, a, b);
+
+    /* subgroup <G> on curve */
+    // x, y: Base Point G
+    Integer x("0x046B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296");
+    Integer y("0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5");
+    // Creat point G
+    ECP::Point G(x, y);
+    // Order n of group <G>
+    Integer n("0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551");
+    // Cofactors
+    Integer h("0x1");
+
+    // Set ECC parameters
+    CryptoPP::DL_GroupParameters_EC<ECP> curve256(eqcurve256r1, G, n, h);
+
+    // Curve parameters
+    cout << "Group order n = " << curve256.GetGroupOrder() << endl;
+    cout << "Cofactor h = " << curve256.GetCofactor() << endl;
+    cout << "Coefficient a = " << eqcurve256r1.GetA() << endl;
+    cout << "Coefficient b = " << eqcurve256r1.GetB() << endl;
+    cout << "Gx = " << G.x << endl;
+    cout << "Gy = " << G.y << endl;
+
+    // Compute on subgroup <G>
+    ECP::Point Q = curve256.GetCurve().Double(G);
+    cout << "Qx =" << Q.x << endl;
+    cout << "Qy =" << Q.y << endl;
+
+    // Scalar Multiply
+    CryptoPP::Integer k("871.");
+    ECP::Point U = curve256.GetCurve().Multiply(k, G);
+    cout << "Ux =" << U.x << endl;
+    cout << "Uy =" << U.y << endl;
+
+    // Point Addition
+    ECP::Point V = curve256.GetCurve().Add(Q, U);
+    cout << "Vx =" << U.x << endl;
+    cout << "Vy =" << U.y << endl;
+
+    // Point inversion
+    ECP::Point X = curve256.GetCurve().Inverse(G);
+    cout << "Xx =" << X.x << endl;
+    cout << "Xy =" << X.y << endl;
+
+    // Multiple
+    ECP::Point H = curve256.GetCurve().ScalarMultiply(G, k);
+    cout << "Hx =" << H.x << endl;
+    cout << "Hy =" << H.y << endl;
+
+    /* Compute on ECC curve  y^2 = x^3 +ax +b*/
+    ECP::Point I = eqcurve256r1.Add(G, H);
+    cout << "Hx=" << I.x << endl;
+    cout << "Hy=" << I.y << endl;
+}
--- Lab 3-4/ECDH.cpp
+++ Lab 3-4/ECDH.cpp
@@ -3,8 +3,8 @@
 
 /* Source, Sink */
 #include "cryptopp/filters.h"
-using CryptoPP::StringSource;
 using CryptoPP::StringSink;
+using CryptoPP::StringSource;
 
 #include <ctime>
 #include <iostream>
@@ -23,70 +23,76 @@ using CryptoPP::ModularSquareRoot;
 
 #include <cryptopp/ecp.h>
 #include <cryptopp/eccrypto.h>
-using CryptoPP::ECP;    // Prime field p
-using CryptoPP::ECIES;
-using CryptoPP::ECPPoint;
+using CryptoPP::DL_FixedBasePrecomputation;
 using CryptoPP::DL_GroupParameters_EC;
 using CryptoPP::DL_GroupPrecomputation;
-using CryptoPP::DL_FixedBasePrecomputation;
+using CryptoPP::ECIES;
+using CryptoPP::ECP; // Prime field p
+using CryptoPP::ECPPoint;
 
 #include <cryptopp/pubkey.h>
-using CryptoPP::PublicKey;
 using CryptoPP::PrivateKey;
+using CryptoPP::PublicKey;
 
 /* standard curves*/
 #include <cryptopp/asn.h>
-#include <cryptopp/oids.h> // 
+#include <cryptopp/oids.h> //
 namespace ASN1 = CryptoPP::ASN1;
 using CryptoPP::OID;
 // hex convert
 #include <cryptopp/hex.h>
-using CryptoPP::HexEncoder;
 using CryptoPP::HexDecoder;
+using CryptoPP::HexEncoder;
 // File operation
 #include <cryptopp/files.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     AutoSeededRandomPool rng;
-// Contruct standrad curve from OID
-    /* ECC curve */
-    CryptoPP::OID oid= ASN1::secp384r1();
-    /* Create a curve for ECDH*/ 
+
+    // Contruct standrad curve from OID
+    // ECC curve
+    CryptoPP::OID oid = ASN1::secp256r1();
+
+    // Create a curve for ECDH
     CryptoPP::ECDH<ECP>::Domain ecdh(oid);
     CryptoPP::DL_GroupParameters_EC<ECP> curve256;
     curve256.Initialize(oid);
-    /* Create key pairs private key d; public key Q=d.G*/
+
+    // Create key pairs private key d; public key Q = d.G
     CryptoPP::SecByteBlock privA(ecdh.PrivateKeyLength()), pubA(ecdh.PublicKeyLength());
     ecdh.GenerateKeyPair(rng, privA, pubA);
+
     CryptoPP::SecByteBlock privB(ecdh.PrivateKeyLength()), pubB(ecdh.PublicKeyLength());
     ecdh.GenerateKeyPair(rng, privB, pubB);
+
     // Read the private key A
     CryptoPP::Integer prkeyA(privA, sizeof(privA));
-    cout << "private key prkey A=" << prkeyA <<endl;
+    cout << "Private key A = " << prkeyA << endl;
+
     // Read the public key point of A
-    ECP::Point G=ecdh.GetGenerator();
-    ECP::Point QA=curve256.GetCurve().Multiply(prkeyA,G);
-    cout << "Public key QAx=" << QA.x << endl;
-    cout << "Public key QAy=" << QA.y << endl;
+    ECP::Point G = ecdh.GetGenerator();
+    ECP::Point QA = curve256.GetCurve().Multiply(prkeyA, G);
+    cout << "Public key QAx = " << QA.x << endl;
+    cout << "Public key QAy = " << QA.y << endl;
 
     // Read the private key B
     CryptoPP::Integer prkeyB(privB, sizeof(privB));
-    cout << "private key prkey B=" << prkeyB <<endl;
+    cout << "Private key prkey B = " << prkeyB << endl;
+
     // Read the public key point of B
-    ECP::Point QB=curve256.GetCurve().Multiply(prkeyB,G);
-    cout << "Public key QBx=" << QB.x << endl;
-    cout << "Public key QBy=" << QB.y << endl;
+    ECP::Point QB = curve256.GetCurve().Multiply(prkeyB, G);
+    cout << "Public key QBx = " << QB.x << endl;
+    cout << "Public key QBy = " << QB.y << endl;
 
     // Session  key A
-    ECP::Point SKA=curve256.GetCurve().Multiply(prkeyA, QB);
+    ECP::Point SKA = curve256.GetCurve().Multiply(prkeyA, QB);
     // Session  key B
-    ECP::Point SKB=curve256.GetCurve().Multiply(prkeyB, QA);
+    ECP::Point SKB = curve256.GetCurve().Multiply(prkeyB, QA);
 
-    cout << "SKAx=" << SKA.x << endl;
-    cout << "SKBx=" << SKB.x << endl;
-    cout << "SKAy=" << SKA.y << endl;
-    cout << "SKBy=" << SKB.y << endl;
-    // Session key H(SKA||....) 
-
+    cout << "SKAx = " << SKA.x << endl;
+    cout << "SKBx = " << SKB.x << endl;
+    cout << "SKAy = " << SKA.y << endl;
+    cout << "SKBy = " << SKB.y << endl;
+    // Session key H(SKA||....)
 }
