8e454f9c6e64f918759820c8005d05d6a79ca690
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-06-13 16:27:44 +0700

    Enclose functions in the helper namespace

--- Lab 3-4/helper.cpp
+++ Lab 3-4/helper.cpp
@@ -42,202 +42,205 @@ using CryptoPP::PublicKey;
 using CryptoPP::FileSink;
 using CryptoPP::FileSource;
 
-void SetupVietnameseSupport()
+namespace helper
 {
+    void SetupVietnameseSupport()
+    {
 #ifdef __linux__
-    setlocale(LC_ALL, "");
+        setlocale(LC_ALL, "");
 #elif _WIN32
-    _setmode(_fileno(stdin), _O_U16TEXT);
-    _setmode(_fileno(stdout), _O_U16TEXT);
+        _setmode(_fileno(stdin), _O_U16TEXT);
+        _setmode(_fileno(stdout), _O_U16TEXT);
 #endif
-}
-
-/* Convert interger to wstring */
-wstring integer_to_wstring(const CryptoPP::Integer &t)
-{
-    std::ostringstream oss;
-    oss.str("");
-    oss.clear();
-    oss << t;                       // pumb t to oss
-    std::string encoded(oss.str()); // to string
-    std::wstring_convert<codecvt_utf8<wchar_t>> towstring;
-    return towstring.from_bytes(encoded); // string to wstring
-}
-
-/* convert string to wstring */
-wstring string_to_wstring(const std::string &str)
-{
-    wstring_convert<codecvt_utf8<wchar_t>> towstring;
-    return towstring.from_bytes(str);
-}
-
-/* convert wstring to string */
-string wstring_to_string(const std::wstring &str)
-{
-    wstring_convert<codecvt_utf8<wchar_t>> tostring;
-    return tostring.to_bytes(str);
-}
-
-void Save(const string &filename, const BufferedTransformation &bt)
-{
-    FileSink file(filename.c_str());
-    bt.CopyTo(file);
-    file.MessageEnd();
-}
-
-void Load(const string &filename, BufferedTransformation &bt)
-{
-    FileSource file(filename.c_str(), true /*pumpAll*/);
-    file.TransferTo(bt);
-    bt.MessageEnd();
-}
-
-void SaveBase64(const string &filename, const BufferedTransformation &bt)
-{
-    // http://www.cryptopp.com/docs/ref/class_base64_encoder.html
-    Base64Encoder encoder;
-    bt.CopyTo(encoder);
-    encoder.MessageEnd();
-    Save(filename, encoder);
-}
-
-void SavePrivateKey(const string &filename, const PrivateKey &key)
-{
-    ByteQueue queue;
-    key.Save(queue);
-    Save(filename, queue);
-}
-
-void SavePublicKey(const string &filename, const PublicKey &key)
-{
-    ByteQueue queue;
-    key.Save(queue);
-    Save(filename, queue);
-}
-
-void SaveBase64PrivateKey(const string &filename, const PrivateKey &key)
-{
-    ByteQueue queue;
-    key.Save(queue);
-    SaveBase64(filename, queue);
-}
-
-void SaveBase64PublicKey(const string &filename, const PublicKey &key)
-{
-    ByteQueue queue;
-    key.Save(queue);
-    SaveBase64(filename, queue);
-}
-
-void LoadPrivateKey(const string &filename, PrivateKey &key)
-{
-    ByteQueue queue;
-    Load(filename, queue);
-    key.Load(queue);
-}
-
-void LoadPublicKey(const string &filename, PublicKey &key)
-{
-
-    ByteQueue queue;
-    Load(filename, queue);
-    key.Load(queue);
-}
-
-void LoadBase64PrivateKey(const string &filename, PrivateKey &key)
-{
-    throw runtime_error("Not implemented");
-}
-
-void LoadBase64PublicKey(const string &filename, PublicKey &key)
-{
-    throw runtime_error("Not implemented");
-}
-
-void LoadBase64(const string &filename, BufferedTransformation &bt)
-{
-    throw runtime_error("Not implemented");
-}
-
-void PrintKeys(RSA::PrivateKey &privateKey, RSA::PublicKey &publicKey)
-{
-    wcout << "##### RSA parameters #####" << endl;
-    wcout << "Public modulo n = " << integer_to_wstring(publicKey.GetModulus()) << endl;
-    wcout << endl;
-    wcout << "Private prime number p = " << integer_to_wstring(privateKey.GetPrime1()) << endl;
-    wcout << endl;
-    wcout << "Private prime number q = " << integer_to_wstring(privateKey.GetPrime2()) << endl;
-    wcout << endl;
-    wcout << "Public key e = " << integer_to_wstring(publicKey.GetPublicExponent()) << endl;
-    wcout << endl;
-    wcout << "Secret key d = " << integer_to_wstring(privateKey.GetPrivateExponent()) << endl;
-    wcout << endl;
-}
-
-char *string_to_ptr_char(string str)
-{
-    char *c = new char[str.size() + 1];
-    for (int i = 0; i < str.size(); ++i)
-    {
-        c[i] = str[i];
-    }
-    c[str.size()] = '\0';
-    return c;
-}
-
-CryptoPP::Integer ReadIntegerFromConsole(wstring string_holder)
-{
-    wstring wstr_input;
-    wcout << string_holder + L": ";
-    fflush(stdin);
-    getline(wcin, wstr_input);
-    char *c = string_to_ptr_char(wstring_to_string(wstr_input));
-    CryptoPP::Integer num(c);
-    delete[] c;
-    return num;
-}
-
-string ReadPlaintextFromFile(string filename)
-{
-    std::locale loc(std::locale(), new codecvt_utf8<wchar_t>);
-    std::wifstream in_file;
-    in_file.open(filename);
-    if (!in_file.is_open())
-    {
-        wcout << L"Không thể mở file!" << endl;
-        exit(1);
     }
 
-    in_file.imbue(loc);
-    wstring data;
-    wstring line;
-    while (in_file.good())
+    /* Convert interger to wstring */
+    wstring integer_to_wstring(const CryptoPP::Integer &t)
     {
-        getline(in_file, line);
-        data += line;
-    }
-    in_file.close();
-    wcout << "Plaintext: " << data << endl;
-    return wstring_to_string(data);
-}
-
-string ReadCiphertextFromFile(string filename)
-{
-    std::ifstream in_file;
-    in_file.open(filename);
-    if (!in_file.is_open())
-    {
-        wcout << L"Không thể mở file!" << endl;
-        exit(1);
+        std::ostringstream oss;
+        oss.str("");
+        oss.clear();
+        oss << t;                       // pumb t to oss
+        std::string encoded(oss.str()); // to string
+        std::wstring_convert<codecvt_utf8<wchar_t>> towstring;
+        return towstring.from_bytes(encoded); // string to wstring
     }
 
-    string data;
-    string line;
-    while (in_file.good())
+    /* convert string to wstring */
+    wstring string_to_wstring(const std::string &str)
     {
-        getline(in_file, line);
-        data += line;
+        wstring_convert<codecvt_utf8<wchar_t>> towstring;
+        return towstring.from_bytes(str);
     }
-    in_file.close();
-    return data;
+
+    /* convert wstring to string */
+    string wstring_to_string(const std::wstring &str)
+    {
+        wstring_convert<codecvt_utf8<wchar_t>> tostring;
+        return tostring.to_bytes(str);
+    }
+
+    void Save(const string &filename, const BufferedTransformation &bt)
+    {
+        FileSink file(filename.c_str());
+        bt.CopyTo(file);
+        file.MessageEnd();
+    }
+
+    void Load(const string &filename, BufferedTransformation &bt)
+    {
+        FileSource file(filename.c_str(), true /*pumpAll*/);
+        file.TransferTo(bt);
+        bt.MessageEnd();
+    }
+
+    void SaveBase64(const string &filename, const BufferedTransformation &bt)
+    {
+        // http://www.cryptopp.com/docs/ref/class_base64_encoder.html
+        Base64Encoder encoder;
+        bt.CopyTo(encoder);
+        encoder.MessageEnd();
+        Save(filename, encoder);
+    }
+
+    void SavePrivateKey(const string &filename, const PrivateKey &key)
+    {
+        ByteQueue queue;
+        key.Save(queue);
+        Save(filename, queue);
+    }
+
+    void SavePublicKey(const string &filename, const PublicKey &key)
+    {
+        ByteQueue queue;
+        key.Save(queue);
+        Save(filename, queue);
+    }
+
+    void SaveBase64PrivateKey(const string &filename, const PrivateKey &key)
+    {
+        ByteQueue queue;
+        key.Save(queue);
+        SaveBase64(filename, queue);
+    }
+
+    void SaveBase64PublicKey(const string &filename, const PublicKey &key)
+    {
+        ByteQueue queue;
+        key.Save(queue);
+        SaveBase64(filename, queue);
+    }
+
+    void LoadPrivateKey(const string &filename, PrivateKey &key)
+    {
+        ByteQueue queue;
+        Load(filename, queue);
+        key.Load(queue);
+    }
+
+    void LoadPublicKey(const string &filename, PublicKey &key)
+    {
+
+        ByteQueue queue;
+        Load(filename, queue);
+        key.Load(queue);
+    }
+
+    void LoadBase64PrivateKey(const string &filename, PrivateKey &key)
+    {
+        throw runtime_error("Not implemented");
+    }
+
+    void LoadBase64PublicKey(const string &filename, PublicKey &key)
+    {
+        throw runtime_error("Not implemented");
+    }
+
+    void LoadBase64(const string &filename, BufferedTransformation &bt)
+    {
+        throw runtime_error("Not implemented");
+    }
+
+    void PrintKeys(RSA::PrivateKey &privateKey, RSA::PublicKey &publicKey)
+    {
+        wcout << "##### RSA parameters #####" << endl;
+        wcout << "Public modulo n = " << integer_to_wstring(publicKey.GetModulus()) << endl;
+        wcout << endl;
+        wcout << "Private prime number p = " << integer_to_wstring(privateKey.GetPrime1()) << endl;
+        wcout << endl;
+        wcout << "Private prime number q = " << integer_to_wstring(privateKey.GetPrime2()) << endl;
+        wcout << endl;
+        wcout << "Public key e = " << integer_to_wstring(publicKey.GetPublicExponent()) << endl;
+        wcout << endl;
+        wcout << "Secret key d = " << integer_to_wstring(privateKey.GetPrivateExponent()) << endl;
+        wcout << endl;
+    }
+
+    char *string_to_ptr_char(string str)
+    {
+        char *c = new char[str.size() + 1];
+        for (int i = 0; i < str.size(); ++i)
+        {
+            c[i] = str[i];
+        }
+        c[str.size()] = '\0';
+        return c;
+    }
+
+    CryptoPP::Integer ReadIntegerFromConsole(wstring string_holder)
+    {
+        wstring wstr_input;
+        wcout << string_holder + L": ";
+        fflush(stdin);
+        getline(wcin, wstr_input);
+        char *c = string_to_ptr_char(wstring_to_string(wstr_input));
+        CryptoPP::Integer num(c);
+        delete[] c;
+        return num;
+    }
+
+    string ReadPlaintextFromFile(string filename)
+    {
+        std::locale loc(std::locale(), new codecvt_utf8<wchar_t>);
+        std::wifstream in_file;
+        in_file.open(filename);
+        if (!in_file.is_open())
+        {
+            wcout << L"Không thể mở file!" << endl;
+            exit(1);
+        }
+
+        in_file.imbue(loc);
+        wstring data;
+        wstring line;
+        while (in_file.good())
+        {
+            getline(in_file, line);
+            data += line + L'\n';
+        }
+        in_file.close();
+        wcout << "Plaintext: " << data << endl;
+        return wstring_to_string(data);
+    }
+
+    string ReadCiphertextFromFile(string filename)
+    {
+        std::ifstream in_file;
+        in_file.open(filename);
+        if (!in_file.is_open())
+        {
+            wcout << L"Không thể mở file!" << endl;
+            exit(1);
+        }
+
+        string data;
+        string line;
+        while (in_file.good())
+        {
+            getline(in_file, line);
+            data += line;
+        }
+        in_file.close();
+        return data;
+    }
 }
